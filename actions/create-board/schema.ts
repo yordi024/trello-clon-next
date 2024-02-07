4039ae9acffb50af0d6a4f0320b2d139244a206f
Yordi Cartagena <yordicartagena24@gmail.com>
2024-02-07 19:44:34 -0400

    Add use action hook and server action wrapper

---
+++ actions/create-board/schema.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod';
+
+export const CreateBoard = z.object({
+  title: z
+    .string({
+      required_error: 'Title is required',
+      invalid_type_error: 'Title is required',
+    })
+    .min(3, {
+      message: 'Minimum length of 3 letters is required',
+    }),
+});
